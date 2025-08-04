782d35e3af6da0174b59125a6ea4375aa39cb0ae
Satyam Kumar Pankaj <satyam07a@gmail.com>
2025-08-04 19:05:05 +0530

    modify

--- app.js
+++ app.js
@@ -16,39 +16,38 @@ const winnPatterns=[
     [6,7,8],
     ];
     const newGame=()=>{
-        ruenO= true;
+        turnO= true;
         enableBoxes();
         win.classList.add("hide");
     };
     
     
-    boxes.forEach((box) => {
-        box.addEventListener("click",() => {
-         //   console.log("Button was clicked ");
-            if (turnO) {// player O
-                box.innerText="O";
-                turnO=false;
+    boxes.forEach((box)=> {
+        box.addEventListener("click", ()=>{
+            //   console.log("Button was clicked ");
+            if (turnO) { // player O
+                box.innerText = "O";
+                turnO = false;
+            } else { //player X
+                box.innerText = "X";
+                turnO = true;
             }
-            else{ //player X
-                box.innerText="X";
-                turnO= true;
-            };
-            boxes.disabled=true;
+            box.disabled = true; // Disable only the clicked box
             checkWinner();
         });
     });
     
-    const disableBoxes= ()=>{
-        for (box of boxes ) {
-            box.disabled=true;
-        };
+    const disableBoxes = ()=>{
+        for (box of boxes) {
+            box.disabled = true;
+        }
     };
-    
-    const enableBoxes= ()=>{
-        for (box of boxes ) {
-            box.disabled=false;
-            box.innerText="";
-        };
+
+    const enableBoxes = ()=>{
+        for (box of boxes) {
+            box.disabled = false;
+            box.innerText = "";
+        }
     };
     
     const showWinner= (winner)=>{
@@ -57,15 +56,15 @@ const winnPatterns=[
         disableBoxes();
     };
     
-    const checkWinner=() =>{
+    const checkWinner=()=>{
         for (let pattern of winnPatterns) {
            
            let pos1Val= boxes[pattern[0]].innerText;
            let pos2Val=boxes[pattern[1]].innerText;
            let pos3Val= boxes[pattern[2]].innerText;
            
-           if (pos1Val!="" && pos2Val!=""&& pos3Val!="") {
-               if (pos1Val=== pos2Val &&  pos2Val===pos3Val) {
+           if (pos1Val!="" && pos2Val!="" && pos3Val!="") {
+               if (pos1Val===pos2Val &&  pos2Val===pos3Val) {
                    disableBoxes();
                    showWinner(pos1Val);
                };
